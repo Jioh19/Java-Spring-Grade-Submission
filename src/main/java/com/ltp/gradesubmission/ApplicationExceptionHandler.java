d8857a9dd6fe99838ba483812684ad5ccc949bd2
Jioh19 <jioh1982@gmail.com>
2023-08-20 22:19:39 -0400

    add global exception handler

--- src/main/java/com/ltp/gradesubmission/ApplicationExceptionHandler.java
+++ src/main/java/com/ltp/gradesubmission/ApplicationExceptionHandler.java
@@ -1,21 +1,28 @@
 package com.ltp.gradesubmission;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-
+import com.ltp.gradesubmission.exception.ErrorResponse;
 
 @ControllerAdvice
 public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @Override //identical to method from workbook 8.3
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    @Override // identical to method from workbook 8.3
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+            HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<String> errors = new ArrayList<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> errors.add(error.getDefaultMessage()));
+        return new ResponseEntity<>(new ErrorResponse(errors), HttpStatus.BAD_REQUEST);
     }
 
 }
